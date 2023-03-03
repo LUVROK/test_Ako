3dbbed50752397891935a12dde06ebc586e90e34
PawelPROGR <pawel.2020.navtop@gmail.com>
2023-03-03 12:59:00 +0300

    fix 2

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ import { findMinRow } from "./utils.js";
 const arr = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => Math.floor(Math.random() * 201) - 100));
 
 const { minr } = findMinRow(arr);
-console.table(arr);
 
-console.log(`\nЧисла в строках с минимальным числом помечены звездочкой: ${minr}`);
+let headerString = "    ";
+for (let i = 0; i < arr[0].length; i++) {
+  const columnIndexString = String(i).padStart(4, " ");
+  headerString += "| " + columnIndexString + " ";
+}
+console.log(headerString + "|");
+
+let separatorString = "    ";
+for (let i = 0; i < headerString.length - 4; i++) {
+  separatorString += "-";
+}
+console.log(separatorString);
+
 for (let i = 0; i < arr.length; i++) {
   let row = "";
   let minPositive = 9999;
   let count = 0;
   let signCount = 0;
 
+  let rowString = String(i).padStart(2, " ") + "  | ";
   for (let j = 0; j < arr[i].length; j++) {
-    row += arr[i][j] + " \t";
+    const cellString = String(arr[i][j]).padStart(4, " ");
+    rowString += cellString + " | ";
+
     if (arr[i][j] > 0 && arr[i][j] < minPositive) {
       minPositive = arr[i][j];
     }
@@ -25,24 +39,11 @@ for (let i = 0; i < arr.length; i++) {
     }
   }
 
-  if (minr.includes(i)) {
-    console.log(
-      i +
-        "* \t" +
-        row +
-        `\n| Наименьшее положительное число в строке - ${i}, число - ${minPositive !== 9999 ? minPositive : "none"} \n| ` +
-        `Минимальное количество чисел, которые нужно заменить, чтобы не было 3 положительных или отрицательных чисел подряд - Строка - ${i}, чисел - ${count}`
-    );
-  } else {
-    console.log(
-      i +
-        "  \t" +
-        row +
-        `\n| Наименьшее положительное число в строке - ${i}, число - ${minPositive !== 9999 ? minPositive : "none"} \n| ` +
-        `Минимальное количество чисел, которые нужно заменить, чтобы не было 3 положительных или отрицательных чисел подряд - Строка - ${i}, чисел - ${count}`
-    );
-  }
-  console.log('|')
+  const cellString__minPositive = String(`${minPositive !== 9999 ? minPositive : "none"}`).padStart(4, " ") + "  | ";
+  const cellString__count = String(`${count}`).padStart(4, " ") + "  | ";
+
+  console.log(rowString + (minr.includes(i) ? " *" : "") + `\t|${cellString__minPositive}` + `${cellString__count}`);
+  console.log(separatorString);
 }
 
 console.log();
